377adef16f91082407bdc35cbfd1458ad57694ba
amina-aliyi <youremail@example.com>
2023-10-27 05:45:47 +0200

    final

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,12 @@
+require (dotenv).config()
+
 import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
 headers: {
 								accept: "application/json",
 								Authorization:
-									"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMWUyM2MyYzBjNzUxNjZjY2VkMTBiYmZhM2YwZTQwMiIsInN1YiI6IjY1MDQyMDdiZmZjOWRlMGVlMTc4OTViYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VxwhryjAhxaCyHguLt0v5Q1D8kj_K2vdnBilYT7Ow5s",
+									Process.env.Token
 							},
 })
 
