bbe503c1868458ece0d90f4607764acdf43233cc
amina-aliyi <youremail@example.com>
2023-12-04 06:17:36 +0200

    updated banner

--- src/axios.js
+++ src/axios.js
@@ -1,15 +1,14 @@
-require (dotenv).config()
-
 import axios from "axios";
+import dotenv from "dotenv";
+// require("dotenv").config();
+dotenv.config();
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
-headers: {
-								accept: "application/json",
-								Authorization:
-									Process.env.Token
-							},
-})
-
+	baseURL: "https://api.themoviedb.org/3",
+	headers: {
+		accept: "application/json",
+		Authorization: process.env.Token,
+	},
+});
 
 export default instance;
 // instance.get("/movie/top_rated");
